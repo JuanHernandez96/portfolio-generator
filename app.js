629dd5c25050fc4245b0f9e092df4234c54779df
Juan Hernandez <juanh22@gmail.com>
2021-07-27 01:30:08 -0600

    added final code so it can work properly

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-const fs = require('fs');
+const { writeFile, copyFile } = require('./utils/generate-site.js');
 const inquirer = require('inquirer');
 const generatePage = require('./src/page-template');
 
@@ -126,15 +126,20 @@ Add a New Project
     });
 };
 promptUser()
-.then(promptProject)
-.then(portfolioData => {
-  const pageHTML = generatePage(portfolioData);
-
-fs.writeFile('./index.html', pageHTML, err => {
-  if (err) throw new Error (err);
-
-  console.log('Portfolio complete! Check out index.html to see the output!')
-});
-
-});
-
+  .then(promptProject)
+  .then(portfolioData => {
+    return generatePage(portfolioData)
+  })
+  .then(pageHTML => {
+    return writeFile(pageHTML);
+  })
+  .then(writeFileReponse => {
+    console.log(writeFileReponse);
+    return copyFile();
+  })
+  .then(copyFileResponse => {
+    console.log(copyFileResponse);
+  })
+  .catch(err => {
+    console.log(err);
+  });
